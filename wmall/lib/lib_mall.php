000e2f40a152edd552831e806c1cc264f180d687
wise4edu <30869488+wise4edu@users.noreply.github.com>
2017-11-06 15:37:16 +0800

    Add files via upload

--- wmall/lib/lib_mall.php
+++ wmall/lib/lib_mall.php
@@ -12,7 +12,7 @@ function list:
   
 $MALL_WWW_RT=$WMALL['MALL_WWW_RT'];
 $MALL_RT=$WMALL['MALL_RT'];
-require_once("$MALL_RT/vwmldbm/lib/lib_code.php");
+require_once("$MALL_RT/vwmldbm/config.php");
 function menu(){
 	global $wmlang;
 	if($_GET['vwmldbm_lang']) $_SESSION['vwmldbm_lang']=$_GET['vwmldbm_lang'];
@@ -22,8 +22,7 @@ function menu(){
 		[<a href='$MALL_WWW_RT/product/'>".$wmlang[menu][prod_list]."</a>] 
 		[<a href='$MALL_WWW_RT/sales/'>".$wmlang[menu][sales_list]."</a>] 
 		";
-	$inside_tag="onChange=\"window.location='$_SELF?vwmldbm_lang='+this.value\"";	
-	\vwmldbm\code\print_lang($_SESSION['vwmldbm_lang'],null,true,$inside_tag);
+	\vwmldbm\code\mlang_change_list(); // multi-lang list and change the setting
 	echo "</p>";
 }
 
